a3f73b4de36b5633cd57c06bb78c09e6164e755e
Kiet Ho <hoakiet98@gmail.com>
2023-03-04 15:04:37 -0600

    Improve layer tile

--- lib/screens/layer_popover.dart
+++ lib/screens/layer_popover.dart
@@ -31,13 +31,37 @@ class LayerPopover extends HookWidget {
   Widget build(BuildContext context) {
     Widget getDeleteButton(index) {
       if (index != activeLayerIndex.value) {
-        return IconButton(
-          icon: const Icon(
-            FontAwesomeIcons.trash,
-            size: 15,
+        return InkWell(
+          child: const Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Icon(
+              FontAwesomeIcons.trash,
+              size: 15,
+            ),
           ),
-          onPressed: () {
+          onTap: () {
             onRemoveLayer(index);
+          },
+        );
+      } else {
+        return const SizedBox.shrink();
+      }
+    }
+
+    Widget getVisibilityButton(index) {
+      if (index != activeLayerIndex.value) {
+        return InkWell(
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Icon(
+              layers.value[index].isVisible
+                  ? FontAwesomeIcons.eye
+                  : FontAwesomeIcons.eyeSlash,
+              size: 15,
+            ),
+          ),
+          onTap: () {
+            onToggleLayerVisibility(index);
           },
         );
       } else {
@@ -52,126 +76,108 @@ class LayerPopover extends HookWidget {
       return Image.memory(layers.value[index].image);
     }
 
-    Widget getVisibilityButton(index) {
-      if (index != activeLayerIndex.value) {
-        return IconButton(
-          icon: Icon(
-            layers.value[index].isVisible
-                ? FontAwesomeIcons.eye
-                : FontAwesomeIcons.eyeSlash,
-            size: 15,
-          ),
-          onPressed: () {
-            onToggleLayerVisibility(index);
-          },
-        );
-      } else {
-        return const SizedBox.shrink();
-      }
-    }
-
     return ValueListenableBuilder(
         valueListenable: layers,
         builder: (context, value, child) {
-          return Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Row(
-                  children: [
-                    const Text(
-                      "Layers",
-                      style: TextStyle(
-                        fontSize: 22,
-                        fontWeight: FontWeight.bold,
+          return Column(children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Row(
+                children: [
+                  const Text(
+                    "Layers",
+                    style: TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const Spacer(),
+                  IconButton(
+                    icon: const Icon(
+                      FontAwesomeIcons.plus,
+                      color: Colors.white,
+                      size: 20,
+                    ),
+                    onPressed: onAddLayer,
+                  ),
+                ],
+              ),
+            ),
+            ReorderableListView(
+              reverse: true,
+              shrinkWrap: true,
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              buildDefaultDragHandles: false,
+              onReorder: (int oldIndex, int newIndex) {
+                onMoveLayer(oldIndex, newIndex);
+              },
+              header: Card(
+                color: Colors.grey[800],
+                child: ListTile(
+                  leading: Container(
+                    width: 40,
+                    height: 30,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(5),
+                      border: Border.all(
+                        color: Colors.grey,
+                        width: 1,
                       ),
                     ),
-                    const Spacer(),
-                    IconButton(
-                      icon: const Icon(
-                        FontAwesomeIcons.plus,
-                        color: Colors.white,
-                        size: 20,
-                      ),
-                      onPressed: onAddLayer,
+                  ),
+                  title: const Text(
+                    "Background color",
+                    style: TextStyle(
+                      color: Colors.white,
                     ),
-                  ],
+                  ),
+                  onTap: () {
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        // TODO: Add previous colors
+                        return AlertDialog(
+                          title: const Text(
+                            'Choose background color',
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          content: SingleChildScrollView(
+                            child: ColorPicker(
+                              pickerColor: Colors.white,
+                              onColorChanged: (value) {
+                                // TODO: Set background color
+                                print(value);
+                              },
+                              pickerAreaBorderRadius: const BorderRadius.all(
+                                Radius.circular(10),
+                              ),
+                            ),
+                          ),
+                          actions: <Widget>[
+                            TextButton(
+                              child: const Text('Done'),
+                              onPressed: () => Navigator.pop(context),
+                            ),
+                          ],
+                        );
+                      },
+                    );
+                  },
                 ),
               ),
-              ReorderableListView(
-                reverse: true,
-                shrinkWrap: true,
-                padding: const EdgeInsets.symmetric(horizontal: 40),
-                buildDefaultDragHandles: false,
-                onReorder: (int oldIndex, int newIndex) {
-                  onMoveLayer(oldIndex, newIndex);
-                },
-                header: Card(
-                  color: Colors.grey[800],
-                  child: ListTile(
-                    leading: Container(
-                      width: 40,
-                      height: 30,
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(5),
-                        border: Border.all(
-                          color: Colors.grey,
-                          width: 1,
-                        ),
-                      ),
-                    ),
-                    title: const Text(
-                      "Background color",
-                      style: TextStyle(
-                        color: Colors.white,
-                      ),
-                    ),
-                    onTap: () {
-                      showDialog(
-                        context: context,
-                        builder: (BuildContext context) {
-                          // TODO: Add previous colors
-                          return AlertDialog(
-                            title: const Text(
-                              'Choose background color',
-                              style: TextStyle(fontWeight: FontWeight.bold),
-                            ),
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                            content: SingleChildScrollView(
-                              child: ColorPicker(
-                                pickerColor: Colors.white,
-                                onColorChanged: (value) {
-                                  // TODO: Set background color
-                                  print(value);
-                                },
-                                pickerAreaBorderRadius: const BorderRadius.all(
-                                  Radius.circular(10),
-                                ),
-                              ),
-                            ),
-                            actions: <Widget>[
-                              TextButton(
-                                child: const Text('Done'),
-                                onPressed: () => Navigator.pop(context),
-                              ),
-                            ],
-                          );
-                        },
-                      );
-                    },
-                  ),
-                ),
-                children: <Widget>[
-                  for (int index = 0; index < layers.value.length; index += 1)
-                    Card(
-                      key: Key('$index'),
-                      color: index == activeLayerIndex.value
-                          ? Colors.blue
-                          : Colors.grey[800],
+              children: <Widget>[
+                for (int index = 0; index < layers.value.length; index += 1)
+                  Card(
+                    key: Key('$index'),
+                    color: index == activeLayerIndex.value
+                        ? Colors.blue
+                        : Colors.grey[800],
+                    child: ReorderableDragStartListener(
+                      index: index,
                       child: ListTile(
                         title: Text(layers.value[index].title),
                         selectedColor: Colors.white,
@@ -194,25 +200,27 @@ class LayerPopover extends HookWidget {
                           ),
                           child: getPreviewImage(index),
                         ),
-                        trailing:
-                            Row(mainAxisSize: MainAxisSize.min, children: [
-                          getDeleteButton(index),
-                          getVisibilityButton(index),
-                          ReorderableDragStartListener(
-                            index: index,
-                            child: const Icon(
-                              FontAwesomeIcons.gripLines,
-                              color: Colors.white,
-                              size: 18,
+                        trailing: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            getDeleteButton(index),
+                            getVisibilityButton(index),
+                            const Padding(
+                              padding: EdgeInsets.only(left: 10),
+                              child: Icon(
+                                FontAwesomeIcons.gripLines,
+                                color: Colors.white,
+                                size: 18,
+                              ),
                             ),
-                          ),
-                        ]),
+                          ],
+                        ),
                       ),
                     ),
-                ],
-              )
-            ]),
-          );
+                  ),
+              ],
+            )
+          ]);
         });
   }
 }
